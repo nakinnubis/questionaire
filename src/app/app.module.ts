bc9dfc50dad36b31618229314a6c2cc3ce245736
nakinnubis <abiola.akinnubi@gmail.com>
2018-04-21 22:06:25 +0100

    adding my files to the repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule,MatFormFieldModule,MatInputModule,MatSelectModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import {  QuestionComponent} from "./question.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,QuestionComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule, 
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+  ],  
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/question.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector:'question',
+    templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css'],
+// template: 'The Questionaire App<button md-button>Click me<button>'
+})
+export class QuestionComponent {
+    title = 'Questionaire';
+  }
+  

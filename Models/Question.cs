22eb9106bda45543bc164c5e0572bbf5a0da6d3d
akinnubi sylvester <abiola.akinnubi@gmail.com>
2018-04-24 02:06:03 +0100

    adding backend

---
+++ Models/Question.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace quizapp.Models
+{
+    public class Question{
+        public string Text { get; set; }
+    }
+}
